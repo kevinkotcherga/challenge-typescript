52460f1f10c25dcce634bca44a45aa288239fdc6
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-09-20 10:25:45 +0200

    feat(correction):correction de l'exercice typescript

--- challenge.ts
+++ challenge.ts
@@ -1,6 +1,6 @@
 interface User {
-  name : string;
-  age?: number;
+  name: string;
+  age: number;
   birthday?: string;
 }
 
@@ -13,7 +13,7 @@ const prettyPrintWilder = (users: User[]): void => {
 
 const wilders: User[] = [];
 const user1: User = { name: "Pierre", age: 23 };
-const user2: User = { name: "Paul", birthday: "10/02/1990" };
+const user2: User = { name: "Paul", birthday: "10/02/1990", age:30 };
 const user3: User = { name: "Jacques", age: 25 };
 wilders.push(user1);
 wilders.push(user2);
